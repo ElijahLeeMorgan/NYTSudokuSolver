5778c44d3c0d14ccc766389354b9bb0d60c5e53b
Elijah Morgan <morgaeli@mail.gvsu.edu>
2024-04-17 05:41:20 +0000

    Finished parsing HTML into usable values. Continue parsing values into np.matrix, also consider adding print statements or loading bar to make wait feel faster.

--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
 """
-A simple web scraper that takes the current Sudoku ouzzles from the New York Times website.
+A simple web scraper that takes the current Sudoku puzzles from the New York Times website.
 This isn't the newest or prettiest implementation of Python web-scraping, but it's good to learn.
 """
 from requests import get
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
-from numpy import matrix
+from numpy import matrix, empty
 
 
 class SudokuScrape:
@@ -20,9 +20,9 @@ class SudokuScrape:
 
     def _scrapeCells(self, mode:int) -> list[any]: #FIXME Bad typing I know, but I'm having trouble pinning down the WebElement class.
         self.driver.get('https://www.nytimes.com/puzzles/sudoku/' + self._modes[mode])
-        #sudokuCells = [self.driver.find_element(By.CSS_SELECTOR, f"div.su-cell:nth-child({i})") for i in range(1,82)]
         sudokuCells = [self.driver.find_element(By.XPATH, f"/html/body/div[3]/div[2]/div[2]/div[6]/div[3]/div/div/div[1]/div/div/div/div[{i}]") for i in range(1,82)]
         sudokuCells = self._turnToSoup(sudokuCells)
+        sudokuCells = self._soupToMatrix(sudokuCells)
         return sudokuCells
     
     def _turnToSoup(self, elements:list) -> list[str]: #FIXME Bad typing I know, but I'm having trouble pinning down the WebElement class.
@@ -30,23 +30,30 @@ class SudokuScrape:
             element.click()
         soup = bs(self.driver.page_source, 'html.parser')
         self.driver.quit()
-
-        strainedSoup = [soup.find("div", attrs={"data-cell": str(i)}) for i in range(0,81)]
-        #TODO continue from here.
-    
-        return soup
+        return [soup.find("div", attrs={"data-cell": str(i)}) for i in range(0,81)]
 
 
-    def _soupToWaffle(self, noodles:list[str]) -> matrix[int]:
-        ...
+
+    def _soupToMatrix(self, elements:list[str]) -> matrix[int]:
+        strConvert = {str(i):i for i in range(1,10)}
+        strConvert["empty":0]
+        puzzle = empty(9,9)
+
+        for element in elements:
+            if element.has_attr("aria-label"):
+                index = elements.index(element)
+                value = strConvert[element] #FIXME Cannot hash slices for dict hashing.
+
+                puzzle[value % 9][value // 9]
+            else:
+                raise ValueError
+        return puzzle
 
     def scrape(self, mode:int) -> matrix:
         if 3 < mode < 0:
             raise ValueError
-        sudokuCells = self._scrapeCells(mode)
-
-        puzzle = matrix([0,0,0,0]) #TODO Populate with values from scraped page.
-        return puzzle
+        print(self._scrapeCells(mode))
+        return None 
 
 
 a = SudokuScrape()
